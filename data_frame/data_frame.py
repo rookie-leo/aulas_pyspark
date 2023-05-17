3a7c11f0e7aaa94fb08bbeea852f29d66da85fd5
rookie-leo <proleonardopa@gmail.com>
2023-05-17 20:30:06 -0300

    Operações basicas

--- data_frame/data_frame.py
+++ data_frame/data_frame.py
@@ -1,5 +1,5 @@
 from pyspark.shell import spark
-from pyspark.sql import functions as Func
+from pyspark.sql.functions import *
 
 
 def executar(path: str) -> None:
@@ -7,4 +7,18 @@ def executar(path: str) -> None:
     despachantes = spark.read.csv(f"{path}", header=False, schema=arqschema)
     despachantes.show()
 
-    # despachantes.select("id", "nome", "vendas").where(Func.col("vendas"))
+    despachantes.select("id", "nome", "vendas").where((col("vendas") > 20) & (col("vendas") < 40)).show()
+
+    despachantes2 = despachantes.withColumn("data2", to_timestamp(col("data"), "yyyy-MM-dd"))
+
+    despachantes2.show()
+
+    despachantes2.select(year("data")).show()
+
+    despachantes2.select(year("data")).distinct().show()
+
+    despachantes2.select("nome", year("data")).orderBy("nome").show()
+
+    despachantes2.select("data").groupby(year("data")).count().show()
+
+    despachantes2.select(sum("vendas")).show()
